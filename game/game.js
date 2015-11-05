d4d77782229226cc75aac4cc7f23b6a858e8ce20
KW2811 <bsd-14@BSD-14s-iMac.local>
2015-11-05 15:17:59 +0800

    lbgi

--- game/game.js
+++ game/game.js
@@ -1,1 +1,9 @@
 //gameplay here
+$(document).ready(function() {
+
+  $("#playBtn").click(function() {
+    $("#playBtn").hide();
+    $("#gameBody").css("background", "url('newimg.jpg')");
+  });
+
+});
