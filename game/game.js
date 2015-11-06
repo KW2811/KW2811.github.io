22fe61c30a9181dfee017b7b9fddb9e8594bea7b
KW2811 <bsd-14@BSD-14s-iMac.local>
2015-11-06 14:37:54 +0800

    'projy[54

--- game/game.js
+++ game/game.js
@@ -6,6 +6,23 @@ $(document).ready(function() {
     $("#gameBody").css("background", "url('bg.png')");
     $("#krishnadiv").css("display", "inline-block");
     $("#Pigdiv").css("display", "inline-block");
+    $("#mvkdiv").css("display", "inline-block");
+  });
+
+  $("#up").click(function() {
+  	$("#krishnadiv").css("top", "100px");
+  });
+
+  $("#down").click(function() {
+  	$("#krishnadiv").css("top", "500px");
+  });
+
+  $("#left").click(function() {
+  	$("#krishnadiv").css("left", "10px");
+  });
+
+  $("#right").click(function() {
+  	$("#krishnadiv").css("left", "500px");
   });
 
 });
